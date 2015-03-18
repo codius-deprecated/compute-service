0b56ed16a75a72f8fa9c3e4212887ad53786ef22
Torrie Fischer <torrie@ripple.com>
2015-03-18 16:58:14 -0700

    Ignore vim swaps

--- conf/workers.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var config = require('../lib/config');
-
-module.exports = {
-  workers: config.get('beanstalk:workers'),
-  server: config.get('beanstalk:server'),
-  tubes: ['instance-launch'],
-  handlers: [path.resolve(__dirname, '../workers/launch')]
-};
